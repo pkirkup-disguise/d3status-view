1c8d450f7cc1e1ad8660e2585a89f5395063be0b
pkirkup-disguise <71440017+pkirkup-disguise@users.noreply.github.com>
2023-04-13 12:13:07 +0100

    more status information
    added icons for director/actor/understudy, and added the ability to see if machines are offline with an icon.

--- d3net_status.js
+++ d3net_status.js
@@ -23,30 +23,62 @@ async function fetchData(path, method = 'GET', body = null) {
     }
 }
 
+async function fetchSessionStatus() {
+    return await fetchData('session');
+}
+
 const machineStatusTable = document.getElementById('machineStatusTable');
 const notificationDetails = document.getElementById('notificationDetails');
 
 async function updateMachineStatus() {
     const machineHealthData = await fetchData('health');
     const machineNotificationData = await fetchData('notifications');
+    const sessionStatusData = await fetchSessionStatus();
 
-    if (machineHealthData && machineHealthData.result && machineNotificationData && machineNotificationData.result) {
+    if (machineHealthData && machineHealthData.result && machineNotificationData && machineNotificationData.result && sessionStatusData && sessionStatusData.result) {
+        const directorUid = sessionStatusData.result.director.uid;
+        const actorUids = sessionStatusData.result.actors.map(actor => actor.uid);
+        const understudyUids = sessionStatusData.result.understudies.map(understudy => understudy.uid);
+
         let tableContent = '';
         for (const machineHealth of machineHealthData.result) {
-            const machineRole = machineHealth.runningAsMachine.uid === machineHealth.machine.uid ? 'Director' : 'Actor';
+            const machineUid = machineHealth.machine.uid;
+            let machineRole = 'Unknown';
+            let machineRoleEmoji = '❓';
+            if (machineUid === directorUid) {
+                machineRole = 'Director';
+                machineRoleEmoji = '🎬';
+            } else if (actorUids.includes(machineUid)) {
+                machineRole = 'Actor';
+                machineRoleEmoji = '🎭';
+            } else if (understudyUids.includes(machineUid)) {
+                machineRole = 'Understudy';
+                machineRoleEmoji = '📚';
+            }
+
             const machineNotifications = machineNotificationData.result.find(mn => mn.machine.uid === machineHealth.machine.uid);
             const notificationCount = machineNotifications ? machineNotifications.notifications.length : 0;
-            tableContent += `<tr>
+            
+            const isMachineOffline = machineHealth.status.states.some(state => state.severity === 'offline' && state.category === 'connection');
+            const rowClass = isMachineOffline ? 'offline-machine' : '';
+
+            tableContent += `<tr class="${rowClass}">
                 <td>
                     ${machineHealth.machine.name}
                     ${notificationCount > 0 ? `<span class="notification-circle" onclick="showNotificationDetails('${machineHealth.machine.uid}')">${notificationCount}</span>` : ''}
                 </td>
-                <td>${machineRole}</td>
+                <td>${machineRoleEmoji} ${machineRole}</td>
                 <td>${machineHealth.status.averageFPS.toFixed(2)}</td>
                 <td>${machineHealth.status.videoDroppedFrames}</td>
                 <td>${machineHealth.status.videoMissedFrames}</td>
                 <td>
-                    ${machineHealth.status.states.map(state => `<div class="${state.severity}">${state.name}: ${state.detail}</div>`).join('')}
+                    ${machineHealth.status.states.map(state => {
+                        if (state.severity === 'offline' && state.category === 'connection') {
+                            return `<div class="${state.severity}">⚠️ ${state.name}: ${state.detail || 'Machine is offline'}</div>`;
+                        } else {
+                            return `<div class="${state.severity}">${state.name}: ${state.detail}</div>`;
+                        }
+                    }).join('')}
                 </td>
             </tr>`;
         }
